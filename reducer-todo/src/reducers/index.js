5fa33e9ea9f1dcd1fcac9c1ebb3ea9b730e972e7
myke47 <47667530+myke47@users.noreply.github.com>
2019-11-20 17:23:23 -0700

    Initial reducer state and Todo added

--- reducer-todo/src/reducers/index.js
+++ reducer-todo/src/reducers/index.js
@@ -2,12 +2,21 @@
 
 
 
-
+export const initialState = {
+  todos: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
+    }
+  ]
+};
 
 export const reducer = (state, action) => {
   const newTodo = {
-    item: ""
+    item: "",
     id: Date.now();
+    completed: false
   };
   return {
     todos: [newTodo]
